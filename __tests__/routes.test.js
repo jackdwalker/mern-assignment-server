27e881350561100bedee75afff931522913aa898
RaphaCA <160218@tuta.io>
2019-07-18 13:16:41 +1000

    test folder updated and commented, for get requests on user login

---
+++ __tests__/routes.test.js
@@ -0,0 +1,24 @@
+import request from 'supertest'
+
+// const app = ()
+
+// testing login with correct credentials 
+// it('suceeeds with correct login credentials', async () => {
+//     const response = await post(`login`, testStudent)
+//     .expect(200)
+//     expect(res.body.student.email).toBe(testStudent.email)
+// })
+
+// // test with incorrect credentials
+// it('fails with incorrect credentials', async () => {
+//     const student = {email: 'notarealuser@test.com', password: 'notrealpassword'}
+//         await post(`login`, student)
+//         .expect(400)
+// })
+
+// // test with missing credentials
+// it('fails with missing credentials', async () => {
+//     const user = {}
+//     await post(`login`, user)
+//     .expect
+// })
