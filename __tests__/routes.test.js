560330273fbf7037772a12b12f06726124699d45
RaphaCA <160218@tuta.io>
2019-07-25 15:16:38 +1000

    final testing added

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,11 +1,9 @@
 const supertest = require ('supertest')
-// const jest = require ('jest')
+// const jest = require ('jest') jest not required in this import 
 const student_routes = require ('../routes/student_routes')
 const app = require ('../server')
 
 // testing DB Connection
-describe 
-
 
 // testing the root path 
 describe('test the root path get request', () => {
@@ -17,14 +15,38 @@ describe('test the root path get request', () => {
 
 })
 
-// testing the authenticate_routes
-describe('it should return a 200 for a route that sends back an authorised token',() => {
-    test('it should return a 200 for a authorised token', async () => {
-        const response = await request(app).get()
-    }
-})
+// this test was failing so wrote test below
+// // testing the authenticate_routes 
+// describe('it should return a 200 for a route that sends back an authorised token',() => {
+//     test('it should return a 200 for a authorised token', async () => {
+//         const response = await request(app).get()
+
+//     })
+// })
+
+
+describe("GET /secure/:id", () => {
+    test("authorizes only correct users", async () => {
+      const response = await request(app)
+        // set isLoggedIn flag to true in local storage with the token, and go to the same ID as the one stored in the token
+        .get(`/users/secure/${auth.current_user_id}`)
+        .set("authorization", auth.token);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toBe(res.cookie)
+    })
+  })
+
+
+// testing for a 401 on a user not logged in
+describe("GET /users without auth", () => {
+    test("requires login", async () => {
+      // don't add an authorization header with the token...see what happens!
+      const response = await request(app).get("/users/")
+      expect(response.statusCode).toBe(401);
+      expect(response.body.message).toBe('Unauthorized: Invalid token')
+    })
+  })
 
 
 
-// 
 
